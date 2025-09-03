892a8b319ea423d4de21b71eb5fb9cca4360443d
ymj02349 <ymj02349@gmail.com>
2025-09-03 12:39:32 +0900

    feat: close window after running commands

--- ui/src-tauri/src/lib.rs
+++ ui/src-tauri/src/lib.rs
@@ -1,4 +1,5 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use tauri::WebviewWindow;
+
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
@@ -10,11 +11,16 @@ async fn open_url(url: String) -> Result<(), String> {
         .map_err(|e| e.to_string())
 }
 
+#[tauri::command]
+async fn hide_window(window: WebviewWindow) -> Result<(), String> {
+    window.hide().map_err(|e| e.to_string())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, open_url])
+        .invoke_handler(tauri::generate_handler![greet, open_url, hide_window])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
